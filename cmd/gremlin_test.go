83dcccf373531f09d5e8b23eebaecedf640c4f21
jbrusegaard <jbrusegaard@gmail.com>
2025-09-06 21:26:50 -0500

    add linter

--- cmd/gremlin_test.go
+++ cmd/gremlin_test.go
@@ -1,15 +1,20 @@
 package cmd
 
 import (
+	"app/driver"
+	"fmt"
 	"testing"
 
 	gremlingo "github.com/apache/tinkerpop/gremlin-go/v3/driver"
 )
 
 func TestGremlin(t *testing.T) {
-	_, err := gremlingo.NewDriverRemoteConnection("http://localhost:8182/gremlin")
+	conn, err := gremlingo.NewDriverRemoteConnection("ws://localhost:8182/gremlin")
 	if err != nil {
 		t.Fatalf("Failed to create driver remote connection: %v", err)
 	}
+	defer conn.Close()
+	g := driver.G(conn)
+	fmt.Println(g.V())
 
 }
---
+++ makefile
@@ -0,0 +1,15 @@
+.PHONY: lint
+lint:
+	pre-commit run --all-files
+
+.PHONY: first-setup
+first-setup:
+	brew install golangci/tap/golangci-lint
+	brew upgrade golangci-lint
+	brew install pre-commit
+	brew install go
+	pre-commit install
+
+.PHONY: go-lint
+go-lint:
+	golangci-lint run --fix --allow-parallel-runners
